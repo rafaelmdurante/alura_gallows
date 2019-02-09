825e93510bd870107486e90d12b358a89b695e7e
Rafael Miranda Durante <rafaelmdurante@gmail.com>
2019-02-09 15:58:43 +0000

    controller - functions etapa 1

--- js/controller.js
+++ js/controller.js
@@ -5,18 +5,29 @@ var criaController = function(jogo) {
 
   // consulta jogo.getLacunas() e exibe para o usuário cada lacuna
   var exibeLacunas = function () {
-    console.log('falta implementar');
+    $entrada.empty();
+    jogo.getLacunas().forEach(function(lacuna) {
+      $('<li>')
+        .addClass('lacuna')
+        .text(lacuna)
+        .appendTo($lacunas);
+    });
   };
 
   // muda o texto do placeHolder do campo de entrada
   var mudaPlaceHolder = function (texto) {
-    console.log('falta implementar');
+    $entrada
+      .val('')
+      .attr('placeholder', texto);
   };
 
   // passa para jogo.setPalavraSecreta() o valor digitado pelo jogador e chama o a função `exibeLacunas()` e `mudaPlaceHolder()` definidas no controller.
 
   var guardaPalavraSecreta = function () {
-    console.log('falta implementar');
+    jogo.setPalavraSecreta($entrada.val().trim());
+    $entrada.val('');
+    exibeLacunas();
+    mudaPlaceHolder('chuta a letra');
   };
 
   // faz a associação do evento keypress para capturar a entrada do usuário toda vez que ele teclar ENTER
@@ -26,7 +37,7 @@ var criaController = function(jogo) {
       if (event.which == 13) {
         switch (jogo.getEtapa()) {
           case 1:
-          alert('etapa 1 - falta implementar');
+            guardaPalavraSecreta();
           break;
           case 2:
           alert('etapa 2 - falta implementar');
